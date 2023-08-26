8bf651b9ba299227a31c289c85ea5003e1e3903d
sophia <thadd.schelp@gmail.com>
2023-08-25 21:09:12 -0400

    Big performance improvements

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,196 +1,167 @@
 ﻿using ChessChallenge.API;
-using System.Collections.Generic;
 using System;
-using System.Linq;
-using System.Collections;
 
 public class MyBot : IChessBot {
-    MoveTree moveTree;
-    static bool isWhite;
+    MoveTree _moveTree;
+    static bool _isWhite;
+    static readonly int Moves = 4;
+    static int NumMoves;
+    static int ms;
 
-    public Move Think(Board board, Timer timer) {
-        isWhite = board.IsWhiteToMove;
-        moveTree = new(board, 4);
-
-        moveTree = moveTree.ChooseBestMove(board);
-        Console.WriteLine("Basic eval: " + BasicEvalPos(board));
-        Console.WriteLine("Advanced eval: " + moveTree.treeBase.eval);
-        return moveTree.GetMove();
+    public MyBot() {
+        if (NumMoves > 0)
+            Console.WriteLine(ms / NumMoves + " = Average time per turn so far.");
     }
 
+    public Move Think(Board board, Timer timer) {
+        _isWhite = board.IsWhiteToMove;
+        _moveTree = new(board, Moves);
+
+        _moveTree = _moveTree.ChooseBestMove(board);
+        Console.WriteLine("Basic eval: " + BasicEvalPos(board));
+        Console.WriteLine("Advanced eval: " + _moveTree.TreeBase.Eval);
+        ms += timer.MillisecondsElapsedThisTurn;
+        Console.WriteLine(timer.MillisecondsElapsedThisTurn + "ms | " + Moves);
+        ++NumMoves;
+        return _moveTree.TreeBase.Move;
+    }
 
     // Piece values: null, pawn, knight, bishop, rook, queen, king
-    readonly static int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+    static readonly int[] PieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
 
     // Get the basic piece advantage of the current position
     public static int BasicEvalPos(Board board) {
         int eval = 0;
         foreach(PieceList pieces in board.GetAllPieceLists()) {
-            int isMyPieceMultiplier = (pieces.IsWhitePieceList == isWhite ? 1 : -1);
-            foreach(Piece piece in pieces) {
-                int pieceValue = pieceValues[(int)piece.PieceType];
-                switch(piece.PieceType) {
-                    case PieceType.Pawn:
-                        if (piece.IsWhite)
-                            pieceValue += piece.Square.Rank * 10;
-                        else
-                            pieceValue += (7 - piece.Square.Rank) * 10;
-                        break;
-                    default: break;
-                }
-                eval += pieceValue * isMyPieceMultiplier;
+            int isMyPieceMultiplier = pieces.IsWhitePieceList == _isWhite ? 1 : -1;
+            int pieceValue = PieceValues[(int)pieces.TypeOfPieceInList];
+            int scratch = 0;
+            switch(pieces.TypeOfPieceInList) {
+                case PieceType.Pawn:
+                    foreach(Piece piece in pieces)
+                        scratch += pieces.IsWhitePieceList ? piece.Square.Rank : 7 - piece.Square.Rank;
+                    eval += (pieceValue * pieces.Count + scratch * 10) * isMyPieceMultiplier;
+                    break;
+                default:
+                    eval += pieceValue * pieces.Count * isMyPieceMultiplier;
+                    break;
             }
         }
-        if(board.IsInCheckmate()) {
-            if(isWhite != board.IsWhiteToMove) {
-                return int.MaxValue;
-            } else {
-                return int.MinValue;
-            }
-        }
-        if(board.IsDraw()) eval -= 1000;
-        return eval;
+        return board.IsInCheckmate() ? _isWhite != board.IsWhiteToMove ? int.MaxValue : int.MinValue : board.IsDraw() ? eval - 1000 : eval;
     }
 }
 
 public class Node {
-    public Node?[] children;  // A list of possible moves that can follow this move.
-    public Node? parent;
-    public Move move;
-    public int depth;
-    public int eval;
-    public bool shouldBeCulled;
-    private int exploredChildIndex;
-    private int childIndex;
+    public Node[]? Children;  // A list of possible moves that can follow this move.
+    public Node? Parent;
+    public Move Move;
+    public int Depth;
+    public int Eval;
+    public bool ShouldBeCulled;
+    private int _exploredChildIndex;
+    private int _childIndex;
 
-    public Node(Board board) {
-        eval = -int.MaxValue;
-        depth = 0;
-        parent = null;
-        move = Move.NullMove;
-        children = null;
-        exploredChildIndex = 0;
-        shouldBeCulled = false;
+    public Node() {
+        Eval = Int32.MinValue;
     }
 
-    public Node(Board board, Move move, Node? parent) {
-        this.move = move;
-        this.parent = parent;
-        depth = parent.depth + 1;
+    public Node(Board board, Move move, Node parent) {
+        Move = move;
+        Parent = parent;
+        Depth = parent.Depth + 1;
         board.MakeMove(move);
-        eval = MyBot.BasicEvalPos(board);
+        Eval = MyBot.BasicEvalPos(board);
         board.UndoMove(move);
-        children = null;
-        exploredChildIndex = 0;
-        shouldBeCulled = false;
     }
 
-    public Node? GetParent(int level) {
-        Node? p = parent;
-        while (p != null && level-- > 0) p = p.parent;
-        if (p == null) return this;
-        return p;
-    }
+//    public Node GetParent(int level) {
+//        Node? p = Parent;
+//        while (p != null && level-- > 0) p = p.Parent;
+//        return p ?? this;
+//    }
 
     public Node? GetNextChild() {
-        if (children != null && childIndex < children.Length) 
-            return children[childIndex++];
-        childIndex = 0;
-        return null;
+        return Children == null || (_childIndex %= Children.Length + 1) == Children.Length ? null : Children[_childIndex++];
     }
 
     public Node? GetNextUnexploredChild() {
-        if(children != null && exploredChildIndex < children.Length)
-            return children[exploredChildIndex++];
-        return null;
+        return Children != null && _exploredChildIndex < Children.Length ? Children[_exploredChildIndex++]: null;
     }
 }
 
 class MoveTree {
-    private readonly static Random rng = new();
-    public Node treeBase;
-    private int maxTreeDepth;
-
-    public Move GetMove() {
-        return treeBase.move;
-    }
+    private static readonly Random Rng = new();
+    public Node TreeBase;
+    private int _maxTreeDepth;
 
     public void MakeBranch(Board board, Node node) {
-        System.Span<Move> moves = stackalloc Move[128];
+        Span<Move> moves = stackalloc Move[128];
         board.GetLegalMovesNonAlloc(ref moves);
-        // Move[] moves = board.GetLegalMoves();
-        Array.Resize(ref node.children, moves.Length);
-        for (int i = 0; i < moves.Length; ++i)
-            node.children[i] = new(board, moves[i], node);
+        Array.Resize(ref node.Children, moves.Length);
+        for (int i = 0; i < moves.Length;)
+            node.Children[i] = new(board, moves[i++], node);
     }
 
     // Create a new tree calculated to maxTreeDepth
     public MoveTree (Board board, int maxTreeDepth) {
-        this.maxTreeDepth = maxTreeDepth;
-        treeBase = new(board);
-        Node node = treeBase;
+        _maxTreeDepth = maxTreeDepth;
+        TreeBase = new();
+        Node node = TreeBase;
         Node? nextChild;
         do {
             // Generate all children in this node
-            if (node.depth < maxTreeDepth + treeBase.depth && !ShouldNodeBeCulled(node))
+            if (node.Depth < maxTreeDepth + TreeBase.Depth && !ShouldNodeBeCulled(node))
                 MakeBranch(board, node);
 
             // Find the next unexplored node in the tree
-            while ((nextChild = node.GetNextUnexploredChild()) == null && node.parent != null) {
-                board.UndoMove(node.move);
-                node = node.parent;
+            while ((nextChild = node.GetNextUnexploredChild()) == null && node.Parent != null) {
+                board.UndoMove(node.Move);
+                node = node.Parent;
             }
             if (nextChild != null) {
-                board.MakeMove(nextChild.move);
+                board.MakeMove(nextChild.Move);
                 node = nextChild;
             }
-        } while (nextChild != null && node != treeBase);
+        } while (nextChild != null && node != TreeBase);
     }
 
     public bool ShouldNodeBeCulled(Node node) {
         return false;
-        if (node.shouldBeCulled) return true;
-        Node? parent;
-        if ((parent = node.GetParent(2)) == null) return false;
-        List<int> evalHistory = new() {node.eval, parent.eval};
-        node.shouldBeCulled = evalHistory.Max() + 20 < treeBase.eval;
-        return node.shouldBeCulled;
+//        if (node.ShouldBeCulled) return true;
+//        Node? parent;
+//        if ((parent = node.GetParent(2)) == null) return false;
+//        List<int> evalHistory = new() {node.Eval, parent.Eval};
+//        node.ShouldBeCulled = evalHistory.Max() + 20 < TreeBase.Eval;
+//        return node.ShouldBeCulled;
     }
 
     // Choose the best move to make long-term
     public MoveTree ChooseBestMove(Board board) {
-        Node node = treeBase;
+        Node node = TreeBase;
         Node? nextChild;
         bool ourTurn = false;
-        do{
+        do {
             // Find the next unexplored node in the tree
-            while ((nextChild = node.GetNextChild()) == null && node.parent != null) {
+            while ((nextChild = node.GetNextChild()) == null && node.Parent != null) {
                 // Compare this move's eval to the parent
-                if (!ShouldNodeBeCulled(node) && node != treeBase) {
-                    if(ourTurn)
-                        node.parent.eval = Math.Max(node.parent.eval, node.eval);
-                    else
-                        node.parent.eval = Math.Min(node.parent.eval, node.eval);
-                }
+                if (!node.ShouldBeCulled && node != TreeBase && ourTurn ^ node.Eval < node.Parent.Eval)
+                    node.Parent.Eval = node.Eval;
                 ourTurn ^= true;
-                board.UndoMove(node.move);
-                node = node.parent;
+                board.UndoMove(node.Move);
+                node = node.Parent;
             }
             if (nextChild != null) {
                 ourTurn ^= true;
-                board.MakeMove(nextChild.move);
+                board.MakeMove(nextChild.Move);
                 node = nextChild;
             }
-        } while (nextChild != null && node != treeBase);
-        List<Node> bestMoves = new();
-        int maxEval = treeBase.eval;
-        foreach (Node currentNode in treeBase.children)
-            if (currentNode.eval >= maxEval)
-                bestMoves.Add(currentNode);
-        if (bestMoves.Count == 0)
-            treeBase = treeBase.children[0];
-        else
-            treeBase = bestMoves[rng.Next(bestMoves.Count)];
+        } while (nextChild != null && node != TreeBase);
+        Node[] bestMoves = Array.FindAll(TreeBase.Children, x => x.Eval >= TreeBase.Eval);
+        TreeBase = bestMoves.Length == 0 ?
+            TreeBase.Children[0]
+        :
+            bestMoves[0];
+//            bestMoves[Rng.Next(bestMoves.Length)];
         return this;
     }
 }
